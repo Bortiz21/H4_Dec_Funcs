e45e661bcae7e7bb8b0d41f7acd89e1436e1e9c8
Brandon O <116747639+Bortiz21@users.noreply.github.com>
2023-03-08 00:41:27 -0500

    Added layout for Program

--- main.py
+++ main.py
@@ -3,3 +3,25 @@
 # File Name:   H4_Dec_Funcs.py
 # Function:    This Program reads and writes to files
 
+# Define Functions
+# Menu
+def menu():
+    print(
+        '''
+Enter an operation :\n
+1 Add
+2 Subtract
+3 Multiple
+4 Divide
+0 Quit
+'''
+    )
+# calc
+# get_operand
+# add
+# sub
+# mult
+# div
+
+
+menu()
