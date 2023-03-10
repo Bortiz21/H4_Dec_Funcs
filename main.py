53115af19e17c0213bf7c2ff0355df3d26e6d37b
Brandon O <116747639+Bortiz21@users.noreply.github.com>
2023-03-10 10:42:09 -0500

    Created functions for the program.

--- main.py
+++ main.py
@@ -16,12 +16,51 @@ Enter an operation :\n
 0 Quit
 '''
     )
+    user_choice = input()
+    return user_choice
+
+
 # calc
+def calc():
+    print("TBD")
+
+
 # get_operand
+def get_operand():
+    return input("Enter an operand :")
+
+
 # add
+def add(x, y):
+    return x + y
+
+
 # sub
+def sub(x, y):
+    return x - y
+
+
 # mult
+def mult(x, y):
+    return x * y
+
+
 # div
+def div(x, y):
+    return x / y
 
 
-menu()
+menu_choice = int(menu())
+operand_1 = int(get_operand())
+operand_2 = int(get_operand())
+
+if menu_choice == 1:
+    print("Performing Add")
+    print("Result:", add(operand_1, operand_2))
+
+elif menu_choice == 2:
+    print("Performing Subtract")
+    result = (operand_1 - operand_2)
+    print("Result: ", result)
+else:
+    print("error")
