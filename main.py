d166cdb13a0141a0234ae9dfb1b67bb5835f1f1d
Brandon O <116747639+Bortiz21@users.noreply.github.com>
2023-03-13 23:11:08 -0400

    Added menu val function. Working on calc function.

--- main.py
+++ main.py
@@ -2,7 +2,7 @@
 # Program By:  Brandon Ortiz
 # File Name:   H4_Dec_Funcs.py
 # Function:    This Program reads and writes to files
-
+import sys
 # Define Functions
 # Menu
 def menu():
@@ -21,8 +21,27 @@ Enter an operation :\n
 
 
 # calc
-def calc():
-    print("TBD")
+def calc(x):
+    try:
+        if x == 1:
+            print("Performing Add")
+            print("Result:", add(operand_1, operand_2))
+        elif x == 2:
+            print("Performing Subtract")
+            result = (operand_1 - operand_2)
+            print("Result: ", result)
+        elif x == 3:
+            print("Performing Multiply")
+            result = (operand_1 * operand_2)
+            print("Result: ", result)
+        elif x == 4:
+            print("Performing Division")
+            result = (operand_1 / operand_2)
+            print("Result: ", result)
+        else:
+            print("error")
+    except:
+        print("Invalid input")
 
 
 # get_operand
@@ -50,17 +69,23 @@ def div(x, y):
     return x / y
 
 
-menu_choice = int(menu())
+# input_val
+def menu_val(i):
+    if i < 0 or i > 4:
+        print("Invalid Input, please input a number between 0 - 4.\nTerminating program.")
+    else:
+        calc(i)
+
+
+try:
+    menu_choice = int(menu())
+except:
+    print("Invalid Input")
+    sys.exit(1)
 operand_1 = int(get_operand())
 operand_2 = int(get_operand())
 
-if menu_choice == 1:
-    print("Performing Add")
-    print("Result:", add(operand_1, operand_2))
+test_var_1 = 1
+test_var_2 = 999
 
-elif menu_choice == 2:
-    print("Performing Subtract")
-    result = (operand_1 - operand_2)
-    print("Result: ", result)
-else:
-    print("error")
+menu_val(int(menu_choice))
